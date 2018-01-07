5af8d690dd5a39ebb59920c857dc0e111bd4e9cd
Dominik Rodler <dominik.rodler@gmail.com>
2018-01-07 21:51:36 +0100

    Styling for logged errors

--- src/lib/log.js
+++ src/lib/log.js
@@ -2,5 +2,5 @@ import chalk from 'chalk';
 
 export const log = content => console.log(content);
 export const logDetail = content => console.log(chalk.dim(' ', content));
-export const logError = content => console.log(chalk.red('! Error:'), content);
+export const logError = content => console.log(chalk.bgRed(' Error '), chalk.red(content));
 export const logStatus = content => console.log(chalk.green('>', content));
